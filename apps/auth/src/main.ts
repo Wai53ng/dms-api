2fb14bf28671e1cd704e9bb3e1931508d2e5290e
waiseng <waiseng.ng@outlook.com>
2025-07-19 22:39:58 +0800

    format code

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -3,15 +3,15 @@ import { AuthModule } from './auth.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-    const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-        AuthModule,
-        {
-            transport: Transport.TCP,
-            options: {
-                port: 3001,
-            }
-        }
-    );
-    await app.listen();
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3001,
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
