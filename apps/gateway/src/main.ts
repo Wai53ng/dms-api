820141fe6ea89675c2311c7f1464717938b33d10
waiseng <waiseng.ng@outlook.com>
2025-07-27 18:23:09 +0800

    added cors into gateway

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -9,6 +9,13 @@ async function bootstrap() {
 
   const port = configService.getOrThrow<number>('GATEWAY_PORT');
   const env = configService.getOrThrow<string>('ENV');
+  const origin = configService.getOrThrow<string>('CORS_ORIGIN').split(',');
+
+  app.enableCors({
+    origin,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
