b5c1201dfc028e11975ca684310b5dea3855b1f4
waiseng <waiseng.ng@outlook.com>
2025-11-08 20:07:50 +0800

    initial setup for auth module

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/login')
+  async login(@Body() dto: LoginDto) {
+    return await this.authService.login(dto.username, dto.password);
+  }
+
+  @Post('/logout')
+  async logout() {
+    return await this.authService.logout();
+  }
+}
