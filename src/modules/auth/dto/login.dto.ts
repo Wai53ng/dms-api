77b57e5109fefbb495dd5e6ceac95970480d46a7
waiseng <waiseng.ng@outlook.com>
2025-11-09 12:01:40 +0800

    added min length for login dto

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,9 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
   @IsString()
+  @MinLength(6)
   readonly username: string;
 
   @IsString()
+  @MinLength(6)
   readonly password: string;
 }
