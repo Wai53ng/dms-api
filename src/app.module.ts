18bbfcda62c502a7d7f9d7ccd1caf819517b437d
waiseng <waiseng.ng@outlook.com>
2025-11-15 15:49:26 +0800

    remove typeorm for prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,12 @@ import { AppService } from './app.service';
 import { AuthController } from './modules/auth/auth.controller';
 import { AuthService } from './modules/auth/auth.service';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { dbConfig } from './config/database';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(dbConfig),
   ],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
